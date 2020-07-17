6095cebf7d0335660a52fda9d3fc868ca682961e
milkyklim <10698619+milkyklim@users.noreply.github.com>
2020-07-17 03:53:54 +0200

    add transaction hash, block and timestamp to schema

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,4 +1,10 @@
-import { Grant, Stake, StakingContract, Withdrawal } from '../generated/schema'
+import {
+  Grant,
+  Stake,
+  StakingContract,
+  Transaction,
+  Withdrawal,
+} from '../generated/schema'
 import {
   StakingContractAuthorized,
   TokenGrantCreated,
@@ -6,45 +12,81 @@ import {
   TokenGrantStaked,
   TokenGrantWithdrawn,
 } from '../generated/TokenGrant/TokenGrant'
-import { createId } from './utils'
+import { createId, createTransaction } from './utils'
 
 export function handleStakingContractAuthorized(event: StakingContractAuthorized): void {
+  let transaction = Transaction.load(event.transaction.hash.toHexString())
   let stakingContract = new StakingContract(event.params.stakingContract.toHex())
+
+  if (transaction == null) {
+    transaction = createTransaction(event.transaction, event.block)
+  }
+  stakingContract.transaction = transaction.id
+
+  transaction.save()
   stakingContract.save()
 }
 
 export function handleTokenGrantCreated(event: TokenGrantCreated): void {
+  let transaction = Transaction.load(event.transaction.hash.toHexString())
   let grant = new Grant(event.params.id.toString())
 
-  grant.status = 'ACTIVE'
+  if (transaction == null) {
+    transaction = createTransaction(event.transaction, event.block)
+  }
+  grant.status = 'Active'
+  grant.transaction = transaction.id
+
+  transaction.save()
   grant.save()
 }
 
 export function handleTokenGrantStaked(event: TokenGrantStaked): void {
+  let transaction = Transaction.load(event.transaction.hash.toHexString())
   let stakeId = createId(event.params.grantId, event.transaction.hash)
   // @ts-ignore: assign wrapper object to primitive
   let stake = new Stake(stakeId)
   let grant = Grant.load(event.params.grantId.toString())
 
+  if (transaction == null) {
+    transaction = createTransaction(event.transaction, event.block)
+  }
   stake.grantId = grant.id
   stake.amount = event.params.amount
+  stake.transaction = transaction.id
+
+  transaction.save()
   stake.save()
 }
 
 export function handleTokenGrantWithdrawn(event: TokenGrantWithdrawn): void {
+  let transaction = Transaction.load(event.transaction.hash.toHexString())
   let withdrawalId = createId(event.params.grantId, event.transaction.hash)
   // @ts-ignore: assign wrapper object to primitive
   let withdrawal = new Withdrawal(withdrawalId)
   let grant = Grant.load(event.params.grantId.toString())
 
+  if (transaction == null) {
+    transaction = createTransaction(event.transaction, event.block)
+  }
   withdrawal.grantId = grant.id
   withdrawal.amount = event.params.amount
+  withdrawal.transaction = transaction.id
+
+  transaction.save()
   withdrawal.save()
 }
 
 export function handleTokenGrantRevoked(event: TokenGrantRevoked): void {
+  let transaction = Transaction.load(event.transaction.hash.toHexString())
   let grant = Grant.load(event.params.id.toString())
 
-  grant.status = 'REVOKED'
+  if (transaction == null) {
+    transaction = createTransaction(event.transaction, event.block)
+  }
+  grant.status = 'Revoked'
+  grant.transaction = transaction.id
+
+  transaction.save()
   grant.save()
 }
--- src/utils.ts
+++ src/utils.ts
@@ -1,6 +1,16 @@
-import { BigInt, Bytes } from '@graphprotocol/graph-ts'
+import { BigInt, Bytes, ethereum as eth } from '@graphprotocol/graph-ts'
+
+import { Transaction } from '../generated/schema'
 
 // make a derived ID from transaction hash and big number
 export function createId(n: BigInt, tx: Bytes): String {
   return n.toHex() + '-' + tx.toHex()
+}
+
+export function createTransaction(tx: eth.Transaction, block: eth.Block): Transaction {
+  let transaction = new Transaction(tx.hash.toHexString())
+  transaction.timestamp = block.timestamp
+  transaction.block = block.number
+
+  return transaction
 }
